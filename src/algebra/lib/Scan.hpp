d166184f68404e43b013cf46150f4ba569b5ddac
marcos <marcos30004347@gmail.com>
2021-07-16 00:43:24 -0300

    scan array working

--- src/algebra/lib/Scan.hpp
+++ src/algebra/lib/Scan.hpp
@@ -47,15 +47,18 @@ void scan_blocks(
 	block_scan_kernel->setKernelArgument(2, BUFFER_ARG_SIZE, sum_inp.computeUnitRef());
 	block_scan_kernel->setKernelArgument(3, power_of_two*sizeof(int), nullptr);
 	block_scan_kernel->setKernelArgument(4, sizeof(int), &n);
-	
+
+	// Scan individual blocks	
 	block_scan_kernel->enqueue({ (u32)power_of_two }, { (u32)block_size });
 
+	// Scan the sum blocks	
 	scan_rec(&sum_inp, &sum_out, num_blocks);
 
 	add_sums_kernel->setKernelArgument(0, BUFFER_ARG_SIZE, out->computeUnitRef());
 	add_sums_kernel->setKernelArgument(1, BUFFER_ARG_SIZE, out->computeUnitRef());
 	add_sums_kernel->setKernelArgument(2, BUFFER_ARG_SIZE, sum_out.computeUnitRef());
 	
+	// Add each element of the scaned sum block to the elements of its respective block	
 	add_sums_kernel->enqueue({ (u32)power_of_two }, { (u32)block_size });
 }
 
