6202e3b49aa9d1e174e9fdcebca35c0f789f61bf
maTh51 <matheustigon@gmail.com>
2021-08-19 23:27:22 -0300

    finalizando gaussian blur

--- src/gaussian/GaussianBlur.cpp
+++ src/gaussian/GaussianBlur.cpp
@@ -26,128 +26,31 @@ GaussianBlur::GaussianBlur(float sigma, Buffer *pixels, Image* img) {
 
 GaussianBlur::~GaussianBlur(){}
 
-f32 erfCustom(f32 x) {
-    const f32 a1 = 0.254829592;
-    const f32 a2 = -0.284496736;
-    const f32 a3 = 1.421413741;
-    const f32 a4 = -1.453152027;
-    const f32 a5 = 1.061405429;
-    const f32 p = 0.3275911;
-
-    // A&S formula 7.1.26
-    const f32 t = 1.0 / (1.0 + p * abs(x));
-    const f32 y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * exp(-x * x);
-
-    debug(t);
-    debug(y);
-
-    if (x > 0) {
-        x = 1;
-    } else if (x < 0) {
-        x = -1;
-    } else {
-        x = 0;
-    }
-
-    return x * y;
-}
-
-f32 defIntGaussian(f32 x, f32 mu, f32 sigma) {
-    const f32 SQRT2 = 1.414;
-    return 0.5 * erfCustom((x - mu) / (SQRT2 * sigma));
-}
-
-void gaussianKernel(f32 *coeff, u64 kernelSize, f32 sigma) {
-    kernelSize = kernelSize*kernelSize;
-    f32 mu = 0;
-    f32 step = 1;
-    
-    const f32 end = 0.5 * kernelSize;
-    const f32 start = -end;
-
-    f32 sum = 0;
-    f32 x = start;
-    f32 lastInt = defIntGaussian(x, mu, sigma);
-    f32 acc = 0;
-
-    int idx = 0;
-
-    while (x < end) {
-        x += step;
-        f32 newInt = defIntGaussian(x, mu, sigma);
-        f32 c = newInt - lastInt;
-        
-        coeff[idx] = c;
-        
-        sum += c;
-        lastInt = newInt;
-        idx++;
-    }
-
-    // normalize
-    sum = 1/sum;
-    for (int i=0; i<idx+1; i++) {
-        coeff[i] *= sum;
-    }
-}
-
-
-void GaussianBlur::calculateKernel(f32 *GKernel) {
-    // float sigma = this->sigma;
-    
-    // int cKernelSize = pow(kernelDimension, 2);
-    // float *ckernel = new float[kernelDimension*kernelDimension];
- 
-    // float acc = 0;
-    
-    // for (int j = 0; j <= kernelDimension; j++) {
-    //     int y = j - (kernelDimension / 2);
-        
-    //     for (int i = 0; i <= kernelDimension; i++) {
-    //         int x = i - (kernelDimension / 2);
-    //         ckernel[j*kernelDimension+i] = (1 / (2 * 3.14159*pow(sigma, 2)))*exp(-((pow(x, 2) + pow(y, 2)) / (2 * pow(sigma, 2)))); 
-    //         acc += ckernel[j*i+i];
-    //     }
-    // }
-    
-    // for (int j = 0; j <= kernelDimension; j++) {
-    //     for (int i = 0; i <= kernelDimension; i++) {
-    //         ckernel[j*kernelDimension + i] = ckernel[j*kernelDimension + i] / acc;
-    //     }
-    // }
-    
-
-    // return ckernel;
-
-    
-    this->kernelDimension = 5;
-    // initialising standard deviation to 1.0
+void GaussianBlur::calculateKernel(f32 *GKernel)
+{
+    i64 k = this->kernelDimension;
     double sigma = this->sigma;
     double r, s = 2.0 * sigma * sigma;
  
     // sum is for normalization
     double sum = 0.0;
  
-    // generating 5x5 kernel
-    for (int x = -2; x <= 2; x++) {
-        for (int y = -2; y <= 2; y++) {
+    for (int x = -k/2; x <= k/2; x++) {
+        for (int y = -k/2; y <= k/2; y++) {
             r = sqrt(x * x + y * y);
-            GKernel[(x + 2)*this->kernelDimension + y + 2] = (exp(-(r * r) / s)) / (M_PI * s);
-            sum += GKernel[(x + 2)*this->kernelDimension + y + 2];
+            printf("%f ~ %f",(M_PI * s), (exp(-(r * r) / s)));
+            GKernel[(x + k/2)*k + y + k/2] = (exp(-(r * r) / s)) / (M_PI * s);
+            sum += GKernel[(x + k/2)*k + y + k/2];
         }
     }
  
     // normalising the Kernel
-    for (int i = 0; i < this->kernelDimension; ++i)
-        for (int j = 0; j < this->kernelDimension; ++j)
-            GKernel[i*this->kernelDimension + j] /= sum;
-
+    for (int i = 0; i < k*k; ++i)
+        GKernel[i] /= sum;
 }
 
+
 void GaussianBlur::run(Buffer *out) {
-
-
-    std::cout << "INIT" << std::endl;
 
     u64 kernelDimension = (u64)ceilf(6 * sigma);
     if (kernelDimension % 2 == 0) kernelDimension++;
@@ -155,48 +58,38 @@ void GaussianBlur::run(Buffer *out) {
     this->kernelDimension = kernelDimension;
 
     f32 kernel[kernelDimension * kernelDimension];
-    gaussianKernel(kernel, kernelDimension, this->sigma);
+    calculateKernel(kernel);
     Buffer new_kernel(kernel, sizeof(f32) * (this->kernelDimension * this->kernelDimension), Buffer::READ_WRITE, false);
     
     new_kernel.upload();
 
     // std::cout << "kernel dimension: " << kernelDimension << std::endl;
-    // for (int i=0; i<kernelDimension*kernelDimension; i++) {
-    //     //std::cout << kernel[i] << ", " << std::endl;
-    //     u64 aux =  i/kernelDimension;
-    //     u64 aux_2 = i%kernelDimension;
-    //     printf("%ld, %ld, %f\n ", aux, aux_2, kernel[aux * kernelDimension + aux_2]);
+    // for (int i=0; i<kernelDimension; i++) {
+    //     for (int j=0; j<kernelDimension; j++) {
+    //         //std::cout << kernel[i] << ", " << std::endl;
+    //         u64 aux =  i;
+    //         u64 aux_2 = j;
+    //         printf("%f ", kernel[aux * kernelDimension + aux_2]);
+    //     }
+    //     printf("\n");
     // }
 
-    std::cout << "CALC: " << this->numOfChannels << std::endl;
-
     blur_kernel->setKernelArgument(0, this->pixels);
-    std::cout << "A" << std::endl;
     
     blur_kernel->setKernelArgument(1, out);
-    std::cout << "B" << std::endl;
     
     blur_kernel->setKernelArgument(2, new_kernel);
-    std::cout << "C" << std::endl;
 
     blur_kernel->setKernelArgument(3, sizeof(int), &this->rowsSize);
-    std::cout << "D" << std::endl;
     
     blur_kernel->setKernelArgument(4, sizeof(int), &this->colSize);
-    std::cout << "E" << std::endl;
     
     blur_kernel->setKernelArgument(5, sizeof(int), &this->kernelDimension);
-    std::cout << "F" << std::endl;
     
     blur_kernel->setKernelArgument(6, sizeof(int), &this->numOfChannels);
-    std::cout << "G" << std::endl;
 
-    debug(rowsSize);
-    debug(colSize);
     blur_kernel->enqueue({(u64)this->rowsSize * this->colSize * this->numOfChannels}, {1});
     
     out->download();
-    
-    std::cout << "H" << std::endl;
 
 }
--- src/gaussian/kernels/opencl/gaussian_blur.cl
+++ src/gaussian/kernels/opencl/gaussian_blur.cl
@@ -15,31 +15,10 @@ __kernel void blur(
     int x = (idx/(numOfChannels)) % (cols);
     int colorOffset = idx%(numOfChannels);
     float acc=0;
-    
-    // printf("colorOffset = %d\n", colorOffset);
-    // printf("k = %d\n", k);
-    // printf("y = %d\n", y);
-    // printf("x = %d\n", x);
-    // printf("num: %d\n",numOfChannels);
 
-    
     int i, j;
 
     for (int c = 0; c < numOfChannels-1; c++) {
-        // for(j=0;j <=k; j++) {
-        
-        //     int y =y + (j-(k/2));
-        //     if(y < 0 || y >= rows) y = y; 
-
-        //     for(i=0;i <= k; i++) {
-                
-        //         int x = x +(i-(k/2));
-        //         if(x < 0 || x > cols) x = x;
-        //         acc += (float) ((float)(pixels[((y*(cols)+x))*numOfChannels + colorOffset])* gaussian[(j*(k))+i]) ;
-        //         //printf("acc: %d, ", acc);
-        //     }
-            
-        // }
 
         int rowStart = max(0, y - k/2);
         int rowEnd = min(rows, y + k/2);
@@ -53,22 +32,14 @@ __kernel void blur(
         int kernelRowStart = min(y, k/2);
         int kernelRowEnd = min(rows - y, k/2);
 
-        //printf("%d, %d, %d, %d\n ", kernelRowStart, kernelRowEnd, kernelColStart, kernelColEnd);
         for (int row= k/2 - kernelRowStart; row < k/2 + kernelRowEnd; row++) {
             for (int col= k/2 - kernelColStart; col < k/2 + kernelColEnd; col++) {
                 acc += gaussian[row*k+col] * pixels[((y-kernelRowStart + row) * cols + (x-kernelColStart + col)) * numOfChannels + c];
-                //printf("%d, %d, %d\n ", row, col, gaussian[row*k+col]);
             }
         }
-        //acc /= (k) * (k);
         out[(y * cols + x) * numOfChannels + c] = (unsigned char)acc;
     }
 
     out[(y * cols + x) * numOfChannels + numOfChannels - 1] = 255;
 
-    //printf("%d, %d\n", pixels[(y * cols + x) * numOfChannels], pixels[(y * cols + x) * numOfChannels + 1]);
-    //if(acc >= 255) acc = 255;
-    //out[(y * cols + x) * numOfChannels + numOfChannels] = (unsigned char)255;
-
-    //out[idx] = pixels[idx];
 }
