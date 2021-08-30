9e6adf11f2607d9796c5abd3be87731029c3ccf3
luiz-couto <luiz.felipe.556@outlook.com>
2021-08-30 16:22:45 -0300

    uma gota de esperança

--- src/point-cloud/PointCloud.cpp
+++ src/point-cloud/PointCloud.cpp
@@ -34,38 +34,83 @@ void PointCloud::run() {
         this->allMatches.push_back(aux);
     }
 
-    u64 idx = 0;
-    std::vector<std::pair<u64,u64>> lista;
-    std::map<u64,std::vector<std::pair<u64,u64>>>::iterator it;
-    for (it=this->allMatches[0]->keyMap.begin(); it!=this->allMatches[0]->keyMap.end();  it++) {
-        lista.push_back({it->first, idx});
-        idx++;
-    }
+    // u64 idx = 0;
+    // std::vector<std::pair<u64,u64>> lista;
+    // std::map<u64,std::vector<std::pair<u64,u64>>>::iterator it;
+    // for (it=this->allMatches[0]->keyMap.begin(); it!=this->allMatches[0]->keyMap.end();  it++) {
+    //     lista.push_back({it->first, idx});
+    //     idx++;
+    // }
+    
     // para cada imagem
-    for (int i=1; i<this->sifts.size(); i++) {
+    // for (int i=1; i<this->sifts.size(); i++) {
 
-        // para cada keymap dentro de cada PointAux
-        for (it=this->allMatches[i]->keyMap.begin(); it!=this->allMatches[i]->keyMap.end();  it++) {
-            bool flag = false;
+    //     // para cada keymap dentro de cada PointAux
+    //     for (it=this->allMatches[i]->keyMap.begin(); it!=this->allMatches[i]->keyMap.end();  it++) {
+    //         bool flag = false;
 
-            // para cada match do keypoint
-            for(int j=0; j<it->second.size(); j++) {
-                flag = false;
+    //         // para cada match do keypoint
+    //         for(int j=0; j<it->second.size(); j++) {
+    //             flag = false;
 
-                // se deu match com alguma imagem anterior, colocar indice do keypoint "anterior"
-                if(it->second[j].second < i) {
-                    flag = true;
-                    lista.push_back();
-                    break;
-                }
+    //             // se deu match com alguma imagem anterior, colocar indice do keypoint "anterior"
+    //             if(it->second[j].second < i) {
+    //                 flag = true;
+    //                 lista.push_back();
+    //                 break;   
+    //             }
+    //         }
+
+    //         // senao, keypoint novo, add na lista
+    //         if(!flag){
+    //             lista.push_back({it->first, idx});
+    //             idx++;
+    //         }
+    //     }
+    // }
+
+    std::vector<std::vector<u64>> globalKeypoints;
+
+    // Passando por cada Point Aux e criando um vetor com o tamanho de todos os keypoints que deram match
+    for (int i=0; i<this->allMatches.size(); i++) {
+        std::map<u64,std::vector<std::pair<u64,u64>>> keyMap = allMatches[i]->keyMap;
+        std::vector<u64>  vect(keyMap.size(), -1);
+        globalKeypoints.push_back(vect);
+    }
+
+    u64 idx = 0;
+    for (int i=0; i<this->allMatches.size(); i++) {
+        std::map<u64,std::vector<std::pair<u64,u64>>> keyMap = allMatches[i]->keyMap;
+        std::map<u64,std::vector<std::pair<u64,u64>>>::iterator it;
+        
+        for (it = keyMap.begin(); it != keyMap.end(); it++) {
+            u64 keypoint = it->first;
+            std::vector<std::pair<u64,u64>> keypointMatches = it->second;
+
+            if (globalKeypoints[i][keypoint] == -1) {
+                globalKeypoints[i][keypoint] = idx;
+                idx++;
             }
 
-            // senao, keypoint novo, add na lista
-            if(!flag){
-                lista.push_back({it->first, idx});
-                idx++;
+            for (int m=0; m<keypointMatches.size(); m++) {
+                u64 keypointOfImg = keypointMatches[m].first;
+                u64 ImgIdx = keypointMatches[m].second;
+
+                if (globalKeypoints[ImgIdx][keypointOfImg] == -1) {
+                    globalKeypoints[ImgIdx][keypointOfImg] = globalKeypoints[i][keypoint];
+                }
             }
         }
     }
 
 }
+
+
+/*
+    [-1 -1 -1...] []
+    [-1 -1 -1...]
+    1 -> 2
+    2 -> 3
+    1 -> 3
+*/
+
